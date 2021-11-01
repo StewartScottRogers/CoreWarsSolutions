2cfaa1acef55b47aec9a285705074aa10dbeebe5
Stewart Rogers <Stewart.Rogers@PoBox.com>
2021-10-31 17:51:02 -0700

    Extended Memory Core Functionality.

---
+++ CoreWars.Engine.SharedProject/MemoryCore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+using CoreWars.Engine.Extentions;
+
+namespace CoreWars.Engine {
+    internal class MemoryCore {
+        private readonly int BufferChunkSize = 80;
+        private readonly int BufferChunkCount;
+        private readonly byte[] Buffer;
+
+        public MemoryCore(uint eightyByteBufferChunkCount = 1) {
+            BufferChunkCount = (Int32)eightyByteBufferChunkCount;
+            Buffer = new byte[BufferChunkSize * BufferChunkCount];
+        }
+
+        public byte Read(int memoryIndex) 
+            => Buffer[SGetafeMemoryIndex(memoryIndex)];
+
+        public void Write(int memoryIndex, byte memoryByte) =>
+            Buffer[SGetafeMemoryIndex(memoryIndex)] = memoryByte;
+        
+        public int Size { get => Buffer.Length; }
+
+        public override String ToString() {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int bufferSpanIndex = 0; bufferSpanIndex < BufferChunkCount; bufferSpanIndex++) {
+                var bufferSpan = new Span<byte>(Buffer, (bufferSpanIndex * BufferChunkSize), BufferChunkSize);
+                string hexString = bufferSpan.ToArray().ToHexString();
+                stringBuilder.AppendLine(hexString);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private int SGetafeMemoryIndex(int memoryIndex) => (memoryIndex % Buffer.Length);
+    }
+}
--- CoreWars.Engine.TestProject/SmokeUnitTest.cs
+++ CoreWars.Engine.TestProject/SmokeUnitTest.cs
@@ -1,13 +1,22 @@
+using System;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace CoreWars.Engine
-{
+namespace CoreWars.Engine {
     [TestClass]
-    public class SmokeUnitTest
-    {
+    public class SmokeUnitTest {
         [TestMethod]
-        public void TestMethod1()
-        {
+        public void Display_New_MemoryCore_As_String() {
+            Console.WriteLine(new MemoryCore(eightyByteBufferChunkCount: 3).ToString());
+        }
+
+        [TestMethod]
+        public void Display_MemoryCore_Before_And_After_Overwriting_As_String() {
+            MemoryCore memoryCore = new MemoryCore(eightyByteBufferChunkCount: 3);
+            Console.WriteLine(memoryCore.ToString());
+            for (int memoryIndex = 0; memoryIndex < memoryCore.Size; memoryIndex++) 
+                memoryCore.Write(memoryIndex, Convert.ToByte('A'));
+            Console.WriteLine(memoryCore.ToString());
         }
     }
 }
