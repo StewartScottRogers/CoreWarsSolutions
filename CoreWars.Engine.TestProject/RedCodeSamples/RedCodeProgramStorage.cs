e02158a35d7d2607ffedb572247cade47cc703dd
Stewart Rogers <Stewart.Rogers@PoBox.com>
2021-11-03 18:34:47 -0700

    Checkpoint.

--- CoreWars.Engine.TestProject/RedCodeSamples/RedCodeProgramStorage.cs
+++ CoreWars.Engine.TestProject/RedCodeSamples/RedCodeProgramStorage.cs
@@ -6,6 +6,7 @@ namespace CoreWars.Engine.RedCodePrograms {
     internal static class RedCodeProgramStorage {
 
         public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample000() => GetEmbeddedProgram("Example000.redcode");
+
         public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample001() => GetEmbeddedProgram("Example001.redcode");
         public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample002() => GetEmbeddedProgram("Example002.redcode");
         public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample003() => GetEmbeddedProgram("Example003.redcode");
@@ -15,8 +16,8 @@ namespace CoreWars.Engine.RedCodePrograms {
         public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample007() => GetEmbeddedProgram("Example007.redcode");
         public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample008() => GetEmbeddedProgram("Example008.redcode");
         public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample009() => GetEmbeddedProgram("Example009.redcode");
-
         public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample010() => GetEmbeddedProgram("Example010.redcode");
+
         public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample011() => GetEmbeddedProgram("Example011.redcode");
         public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample012() => GetEmbeddedProgram("Example012.redcode");
         public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample013() => GetEmbeddedProgram("Example013.redcode");
@@ -28,6 +29,27 @@ namespace CoreWars.Engine.RedCodePrograms {
         public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample019() => GetEmbeddedProgram("Example019.redcode");
         public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample020() => GetEmbeddedProgram("Example020.redcode");
 
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample021() => GetEmbeddedProgram("Example021.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample022() => GetEmbeddedProgram("Example022.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample023() => GetEmbeddedProgram("Example023.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample024() => GetEmbeddedProgram("Example024.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample025() => GetEmbeddedProgram("Example025.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample026() => GetEmbeddedProgram("Example026.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample027() => GetEmbeddedProgram("Example027.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample028() => GetEmbeddedProgram("Example028.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample029() => GetEmbeddedProgram("Example029.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample030() => GetEmbeddedProgram("Example030.redcode");
+
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample031() => GetEmbeddedProgram("Example031.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample032() => GetEmbeddedProgram("Example032.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample033() => GetEmbeddedProgram("Example033.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample034() => GetEmbeddedProgram("Example034.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample035() => GetEmbeddedProgram("Example035.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample036() => GetEmbeddedProgram("Example036.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample037() => GetEmbeddedProgram("Example037.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample038() => GetEmbeddedProgram("Example038.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample039() => GetEmbeddedProgram("Example039.redcode");
+        public static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetExample040() => GetEmbeddedProgram("Example040.redcode");
 
         private static (string Name, string Cotent, IEnumerable<(int lineNumber, string line)> Codelines) GetEmbeddedProgram(string programName) {
             var assembly = Assembly.GetExecutingAssembly();
--- CoreWars.Engine.TestProject/TokenizerSmokeUnitTest.cs
+++ CoreWars.Engine.TestProject/TokenizerSmokeUnitTest.cs
@@ -42,49 +42,53 @@ namespace CoreWars.Engine {
 
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example000() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample000());
 
+
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example001() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample001());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example002() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample002());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example003() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample003());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example004() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample004());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example005() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample005());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example006() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample006());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example007() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample007());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example008() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample008());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example009() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample009());
-
-
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example010() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample010());
 
+
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example011() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample011());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example012() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample012());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example013() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample013());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example014() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample014());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example015() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample015());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example016() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample016());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example017() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample017());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example018() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample018());
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example019() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample019());
-
-
         [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example020() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample020());
 
-        
+
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example021() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample021());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example022() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample022());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example023() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample023());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example024() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample024());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example025() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample025());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example026() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample026());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example027() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample027());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example028() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample028());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example029() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample029());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example030() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample030());
+
+
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example031() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample031());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example032() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample032());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example033() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample033());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example034() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample034());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example035() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample035());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example036() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample036());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example037() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample037());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example038() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample038());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example039() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample039());
+        [TestMethod] public void Display_Loading_And_Tokenizing_Of_RedCode_Example040() => Display_Loading_And_Tokenizing_Of_RedCode(RedCodeProgramStorage.GetExample040());
+
     }
 }
