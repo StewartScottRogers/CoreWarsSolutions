832860e5ddea8077f2714cff463bef30bcdf0bcf
Stewart Rogers <Stewart.Rogers@PoBox.com>
2021-11-05 08:34:30 -0700

    Checkpoint.

--- CoreWars.Engine.SharedProject/Extentions/LinterExtentions.cs
+++ CoreWars.Engine.SharedProject/Extentions/LinterExtentions.cs
@@ -6,25 +6,26 @@ using CoreWars.Engine.Extentions.Exceptions;
 namespace CoreWars.Engine {
     internal static class LinterExtentions {
 
+        private static Dictionary<String, (String MnemonicType, String Mnemonic, String Description, String Example)> OpcodeDictionary
+               = SymbolLibrary.OpcodeSymbols().ToSymbolsDictionary();
+
+        private static Dictionary<String, (String MnemonicType, String Mnemonic, String Description, String Example)> DirectiveDictionary
+            = SymbolLibrary.DirectiveSymbols().ToSymbolsDictionary();
+
+        private static Dictionary<String, (String MnemonicType, String Mnemonic, String Description, String Example)> AddressDictionary
+            = SymbolLibrary.AddressSymbols().ToSymbolsDictionary();
+
 
         public static IEnumerable<(int LineNumber, string LineType, string Label, string Command, string ParameterA, string ParameterB)>
             LintCodeLines(this IEnumerable<(int LineNumber, string LineType, string Label, string Command, string ParameterA, string ParameterB)> codeLines) {
 
-            Dictionary<String, (String MnemonicType, String Mnemonic, String Description, String Example)> opcodeDictionary 
-                = SymbolLibrary.OpcodeSymbols().ToSymbolsDictionary();
-
-            Dictionary<String, (String MnemonicType, String Mnemonic, String Description, String Example)> addressDictionary 
-                = SymbolLibrary.AddressSymbols().ToSymbolsDictionary();
-
-            Dictionary<String, (String MnemonicType, String Mnemonic, String Description, String Example)> directiveDictionary 
-                = SymbolLibrary.DirectiveSymbols().ToSymbolsDictionary();
-
-
             foreach ((Int32 LineNumber, String LineType, String Label, String Command, String ParameterA, String ParameterB) codeLine in codeLines) {
 
+                if(OpcodeDictionary.ContainsKey(codeLine.Command))
+                    yield return codeLine;
 
-
-
+                if (DirectiveDictionary.ContainsKey(codeLine.Command))
+                    yield return codeLine;
 
 
                 yield return codeLine;
