6dec1138b1ba2cb84dabb1e6cf76333817482c54
Stewart Rogers <Stewart.Rogers@PoBox.com>
2021-11-08 07:42:19 -0700

    Checkpoint.

--- CoreWars.Engine.SharedProject/Extentions/AssemblerExtentions.cs
+++ CoreWars.Engine.SharedProject/Extentions/AssemblerExtentions.cs
@@ -8,7 +8,7 @@ namespace CoreWars.Engine.Extentions {
             => (Name: programName, Cotent: programContent, Codelines: programContent.ToLines());
 
 
-        public static IEnumerable<short> Assemble((string Name, string Cotent, IEnumerable<(short lineNumber, string line)> Codelines) program) {
+        public static IEnumerable<short> AssembleExecutable((string Name, string Cotent, IEnumerable<(short lineNumber, string line)> Codelines) program) {
 
             IEnumerable<(short LineNumber, string LineType, string Label, string Command, string ParameterA, string ParameterB)> parsedCodeLines
                 = program.Codelines.ParseCodeLines();
--- CoreWars.Engine.SharedProject/Extentions/TokenizerExtentions.cs
+++ CoreWars.Engine.SharedProject/Extentions/TokenizerExtentions.cs
@@ -15,12 +15,12 @@ namespace CoreWars.Engine {
         public static IEnumerable<string>
             ToStrings(this IEnumerable<(short LineNumber, string LineType, string Label, string Command, string ParameterA, string ParameterB)> parsedCodeLines) {
 
-            yield return $"{"[Type]",-10} {"[Line]",-8} {"[Label]",-15} {"[Command]",-15} {"[ParameterA]",-20} {"[ParameterB]",-20}";
+            yield return $"{"[Type]",-10} {"[Line]",-8} {"[Label]",-15} {"[Command]",-10} {"[ParameterA]",-20} {"[ParameterB]",-20}";
 
             foreach ((short LineNumber, string LineType, string Label, string Command, string ParameterA, string ParameterB) parsedCodeLine in parsedCodeLines) {
                 string lineNumber = $"{parsedCodeLine.LineNumber:00000}";
 
-                string text = $"{parsedCodeLine.LineType,-10} {lineNumber,-8} {parsedCodeLine.Label,-15} {parsedCodeLine.Command,-15} {parsedCodeLine.ParameterA,-20} {parsedCodeLine.ParameterB,-20}";
+                string text = $"{parsedCodeLine.LineType,-10} {lineNumber,-8} {parsedCodeLine.Label,-15} {parsedCodeLine.Command,-10} {parsedCodeLine.ParameterA,-20} {parsedCodeLine.ParameterB,-20}";
                 yield return text;
             }
         }
