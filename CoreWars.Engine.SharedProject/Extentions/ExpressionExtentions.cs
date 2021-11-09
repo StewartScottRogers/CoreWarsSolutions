a7b7b06ed91cf7e6ea9ad77fea9c098bfc06f840
Stewart Rogers <Stewart.Rogers@PoBox.com>
2021-11-08 19:16:26 -0700

    Checkpoint.

--- CoreWars.Engine.SharedProject/Extentions/ExpressionExtentions.cs
+++ CoreWars.Engine.SharedProject/Extentions/ExpressionExtentions.cs
@@ -1,35 +1,211 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CoreWars.Engine.Extentions {
     internal static class ExpressionExtentions {
         public static short Parse(this Dictionary<string, string> labledValuePairDictionary, string expression) {
 
+
+
             return 0;
         }
 
-        // #1
-        /// ExpressionParser p = new ExpressionParser();
-        /// Map vars = new HashMap<String, Double>();
-        /// vars.put("x", 2.50);
-        /// var result = p.eval(" 5 + 6 * x - 1", vars);
-        /// System.out.println(result);
+        public class ExpressionParser {
+            private readonly Stack<char> _expression = new Stack<char>();
+
+            public ExpressionParser(string expression) {
+                foreach (Char c in expression.Reverse()) 
+                    _expression.Push(c);
+            }
+
+            public IExpression Parse() {
+                var left = new Stack<IExpression>();
+                var operands = new Stack<Operand>();
+                var right = new Stack<IExpression>();
+
+                while (HasMoreChars) {
+                    var c = _expression.Peek();
+
+                    if (char.IsDigit(c)) {
+                        var parameter = ReadParameterExpression();
+                        var expression = ParseParameterExpression(parameter);
+                        if (left.Count == right.Count) {
+                            left.Push(expression);
+                        } else {
+                            right.Push(expression);
+                        }
+                    } else if (IsOperand(c)) {
+                        var operand = ParseOperand(_expression.Pop());
+
+                        if (right.Any() && (operand == Operand.Divide || operand == Operand.Multiply)) {
+                            left.Push(right.Pop());
+                        }
+
+                        while (operands.Any() && left.Any() && right.Any()) {
+                            var expression = new OperationExpression(left.Pop(), operands.Pop(), right.Pop());
+
+                            if (left.Count == right.Count) {
+                                left.Push(expression);
+                            } else {
+                                right.Push(expression);
+                            }
+                        }
+
+                        operands.Push(operand);
+                    } else if (IsOpeningBracket(c)) {
+                        _expression.Pop();
+                        var expression = Parse();
+
+                        if (left.Count == right.Count) {
+                            left.Push(expression);
+                        } else {
+                            right.Push(expression);
+                        }
+                    } else if (IsClosingBracket(c)) {
+                        _expression.Pop();
+
+                        while (operands.Any()) {
+                            var expression = new OperationExpression(left.Pop(), operands.Pop(), right.Pop());
+
+                            if (left.Count == right.Count) {
+                                left.Push(expression);
+                            } else {
+                                right.Push(expression);
+                            }
+                        }
+
+                        return left.Pop();
+                    } else if (char.IsWhiteSpace(c)) {
+                        _expression.Pop();
+                    } else {
+                        throw new NotImplementedException($"Cannot parse char '{c}'");
+                    }
+                }
 
 
-        // #2
-        // ExpressionParser parser = new ExpressionParser();
-        // parser.Values.Add("x", 2);
-        // parser.Values.Add("y", 10);
+                while (operands.Any()) {
+                    var expression = new OperationExpression(left.Pop(), operands.Pop(), right.Pop());
 
-        // // Parse once
-        // string func = "x^3+5x^2-3";
-        // parser.Parse(func);
-        // // Fetch expression
-        // Expression expression = parser.Expressions[func];
+                    if (left.Count == right.Count) {
+                        left.Push(expression);
+                    } else {
+                        right.Push(expression);
+                    }
+                }
 
-        // // Evaluate saved expression
-        // double result = parser.EvalExpression(expression);
+                return left.Pop();
+            }
 
+            internal static bool IsOperand(char c) {
+                return c == '-' || c == '+' || c == '*' || c == '/';
+            }
+
+            internal static bool IsOpeningBracket(char c) {
+                return c == '(';
+            }
+
+            internal static bool IsClosingBracket(char c) {
+                return c == ')';
+            }
+
+            internal Operand ParseOperand(char c) {
+                switch (c) {
+                    case '-':
+                        return Operand.Minus;
+                    case '+':
+                        return Operand.Plus;
+                    case '/':
+                        return Operand.Divide;
+                    case '*':
+                        return Operand.Multiply;
+                    default:
+                        throw new NotImplementedException($"Cannot parse {c} as an operand");
+                }
+            }
+
+            internal string ReadParameterExpression() {
+                var c = _expression.Pop();
+                var expression = new StringBuilder();
+                expression.Append(c);
+
+                while (HasMoreChars) {
+                    c = _expression.Peek();
+                    if (char.IsDigit(c) || c == '.') {
+                        c = _expression.Pop();
+                        expression.Append(c);
+                    } else {
+                        return expression.ToString();
+                    }
+                }
+
+                return expression.ToString();
+            }
+
+            private bool HasMoreChars => _expression.Any();
+
+            internal IExpression ParseParameterExpression(string expression) {
+                double value;
+
+                if (!double.TryParse(expression, out value)) {
+                    throw new InvalidOperationException($"Couldn't parse expression {expression} as a double");
+                }
+
+                return new ParameterExpression(value);
+            }
+        }
+
+        public interface IExpression {
+            double Evaluate();
+        }
+
+        public class ParameterExpression : IExpression {
+            public double Parameter { get; }
+
+            public ParameterExpression(double parameter) {
+                Parameter = parameter;
+            }
+
+            public double Evaluate() {
+                return Parameter;
+            }
+        }
+
+        public class OperationExpression : IExpression {
+            public IExpression Left { get; set; }
+            public IExpression Right { get; set; }
+            public Operand Operand { get; }
+
+            public OperationExpression(IExpression left, Operand operand, IExpression right) {
+                Operand = operand;
+                Left = left;
+                Right = right;
+            }
+
+            public double Evaluate() {
+                switch (Operand) {
+                    case Operand.Plus:
+                        return Left.Evaluate() + Right.Evaluate();
+                    case Operand.Minus:
+                        return Left.Evaluate() - Right.Evaluate();
+                    case Operand.Multiply:
+                        return Left.Evaluate() * Right.Evaluate();
+                    case Operand.Divide:
+                        return Left.Evaluate() / Right.Evaluate();
+                    default:
+                        throw new NotImplementedException("Operation not supported");
+                }
+            }
+        }
+
+        public enum Operand {
+            Plus,
+            Minus,
+            Multiply,
+            Divide
+        }
     }
+
 }
+
